e9578161d84fc49bbc2578e219b4d234d30bcfe1
kn <huyhoang8398@gmail.com>
2020-03-16 15:45:43 +0700

    test vietnam statistics

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const https = require('https')
 var unirest = require('unirest');
 
 var body = '';
+var bodyVietnam = '';
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -44,6 +45,21 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       case 'corona':
         var req = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php");
 
+        var reqByCountry = unirest("GET", "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php");
+
+        reqByCountry.headers({
+          "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+          "x-rapidapi-key": process.env.apiKey
+        });
+        reqByCountry.end(function (res) {
+          if (res.error) throw new Error(res.error);
+          bodyVietnam = JSON.parse(res.body);
+          return bodyVietnam;
+          console.log(res.body);
+        });
+        console.log(bodyVietnam);
+        console.log(typeof (bodyVietnam));
+
         req.headers({
           "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
           "x-rapidapi-key": process.env.apiKey
@@ -65,7 +81,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             ':repeat:' + ' ' + 'Recovered: ' + body.total_recovered + '\n' + '\n' +
             ':mask:' + ' ' + 'New cases: ' + body.new_cases + '\n' + '\n' +
             ':skull_crossbones:' + ' ' + 'New Deaths: ' + body.new_deaths + '\n' + '\n' +
-            ':date:' + ' ' + 'Statistic taken at: ' + body.statistic_taken_at
+            '------------------------------------' + '\n' + '\n' +
+            'Current Corona Virus Statistics in Vietnam \n' + '\n' +
+            ':mask:' + ' ' + 'Confirmed: ' + body.total_cases + '\n' + '\n' + 
+            ':skull:' + ' ' + 'Deaths: ' + body.total_deaths + '\n' + '\n' +
+            ':repeat:' + ' ' + 'Recovered: ' + body.total_recovered + '\n' + '\n' +
+
+
+            ':date:' + ' ' + 'Statistic taken at: ' + body.statistic_taken_at + 
         })
         break;
       case 'nlag':
@@ -77,8 +100,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       case 'bald':
         bot.sendMessage({
           to: channelID,
-//          message: '╭∩╮( ° ͜ʖ͡°)╭∩╮ ' + '<@371660303672541197>'
-            message: 'https://media.discordapp.net/attachments/538397759741362179/688128156174909568/69424511_2564724586917511_6843079252783398912_n.png?width=759&height=566'
+          //          message: '╭∩╮( ° ͜ʖ͡°)╭∩╮ ' + '<@371660303672541197>'
+          message: 'https://media.discordapp.net/attachments/538397759741362179/688128156174909568/69424511_2564724586917511_6843079252783398912_n.png?width=759&height=566'
         })
         break;
       case 'dota':
@@ -90,7 +113,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       case 'regret':
         bot.sendMessage({
           to: channelID,
-          message: 'I immediately regret this decision' + ':cold_face:' 
+          message: 'I immediately regret this decision' + ':cold_face:'
         })
         break;
       case 'thanks':
@@ -102,7 +125,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       case 'phicong':
         bot.sendMessage({
           to: channelID,
-          message: 'stfu ' + '<@425894408714452993>' + ':mask:' 
+          message: 'stfu ' + '<@425894408714452993>' + ':mask:'
         })
     }
   }
