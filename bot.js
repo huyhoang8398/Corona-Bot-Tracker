a4310a0f8c527dc8453c53ecce747940588e9e63
kn <huyhoang8398@gmail.com>
2020-03-25 18:51:15 +0700

    new corona stats

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.io');
 const logger = require('winston');
 const unirest = require('unirest');
-const request = request('request')
+const request = require('request')
 
 var body = '';
 let url = "https://corona-stats.online/vn\?format\=json"
