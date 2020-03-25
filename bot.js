22a8ab67383baff7adc554da2b843a1cab603f5c
kn <huyhoang8398@gmail.com>
2020-03-25 19:24:19 +0700

    new corona stats

--- bot.js
+++ bot.js
@@ -64,8 +64,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         });
 
         request({url:url, qs:propertiesObject}, function(err, response, body) {
-            if(err) { console.log(err); return; }
-            console.log("Get response: " + response.statusCode);
+          if(err) { console.log(err); return; }
+          console.log(body);
         });
 
         bot.sendMessage({
